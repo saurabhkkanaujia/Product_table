844902334e0d7d2bfdff9dc96219491c3c6dc16b
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-08 12:02:56 +0530

    Validation Added

--- src/script.js
+++ src/script.js
@@ -4,27 +4,66 @@ function addProduct(){
     var product = insertJson();
     products.push(product);
     console.log(products);
-    table = document.getElementById("table");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    for (var i=0; i<products.length; i++){
-        cell1.innerHTML = products[i].prod_id;
-        cell2.innerHTML = products[i].prod_name;
-        cell3.innerHTML = products[i].prod_price;
-    }
+    display(products);
 }
 
 function insertJson(){
     var p_id = document.getElementById('p_id').value;
     var p_name = document.getElementById('p_name').value;
     var p_price = document.getElementById('p_price').value;
-    console.log(p_id,p_name,p_price);
-    return {
-        'prod_id' : p_id,
-        'prod_name' : p_name,
-        'prod_price' : p_price
+    
+    if (checkValues(p_id, p_name, p_price) && idValidate(p_id, products)){
+        console.log(p_id,p_name,p_price, p_price);
+        return {
+            'prod_id' : p_id,
+            'prod_name' : p_name,
+            'prod_price' : p_price
+        };
     }
+    else{
+        window.alert('Invalid Input or ID Already Taken');
+    }
+    
+    
 }
 
+function idValidate(p_id, products){
+    for (var i = 0; i<products.length; i++){
+        if (p_id == products[i].prod_id){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+function checkValues(p_id, p_name, p_price){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function display(products){
+
+    var html = '<tr>\
+        <th>Product Id</th>\
+        <th>Product Name</th>\
+        <th>Product Price</th>\
+    </tr> ';
+
+    for (var i=0; i < products.length; i++){
+        html += '<tr>\
+        <td>'+products[i].prod_id+'</td>\
+        <td>'+products[i].prod_name+'</td>\
+        <td>'+products[i].prod_price+'</td>\
+        <td><a href ="#" onclick = editForm('+products.prod_id+')>Edit</a></td>\
+    </tr>'
+    }
+    document.getElementById('table').innerHTML = html;
+}
+
+function editForm(){
+
+}
